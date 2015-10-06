38f0a5f02129e5e713af9de878cccaf67523f92c
Huy Ta Ba Thanh <huytbt@gmail.com>
2015-10-06 13:57:13 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ install Git git
 
 # install PHP and Apache
 install PHP apache2 php5 libapache2-mod-php5
-install PHP-Extensions php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-sqlite
+install PHP-Extensions php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 # install webmin
 install Webmin perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
